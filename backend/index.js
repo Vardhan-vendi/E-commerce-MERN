e4423a1697ac80deb34302db89a4f1785ee86626
Vendi Vardhan Babu <vardhanbabuvendi@gmail.com>
2026-07-15 19:28:27 +0530

    Fix CORS for Vercel frontend

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,12 @@ import path from 'path'
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://mvstore-4f6v675su-vardhan-vendis-projects.vercel.app",
+    credentials: true,
+  })
+);
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
